18bb71daea3845cd301ff656f44af279b79e780a
malthe-kea <mati0002@stud.kea.dk>
2025-11-06 09:53:11 +0100

    inint commit

--- src/main/java/Sirens/ISirenRepository.java
+++ src/main/java/Sirens/ISirenRepository.java
@@ -3,8 +3,7 @@ package Sirens;
 import models.Siren;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface ISirenRepository extends JpaRepository<Siren, Integer> {
-    public Optional<Siren> findById(int id);
-}
+public interface ISirenRepository extends JpaRepository<Siren, Integer> {}
--- src/main/java/Sirens/SirenService.java
+++ src/main/java/Sirens/SirenService.java
@@ -3,18 +3,24 @@ package Sirens;
 import jakarta.persistence.EntityNotFoundException;
 import models.Siren;
 
+import java.util.List;
+import java.util.Optional;
+
 public class SirenService {
     ISirenRepository sirenRepository;
 
     public SirenService(ISirenRepository sirenRepository) {
         this.sirenRepository = sirenRepository;
     }
+    public SirenService(){}
 
     public Siren findSirenById(int id){
         return sirenRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Siren not found with id:" + id));
     }
 
-    public SirenService(){
-
+    public List<Siren> findAllSirens(){
+        return sirenRepository.findAll();
     }
+
+
 }
